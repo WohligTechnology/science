3f0f4ba9be8fcdca8cdbedb491bd48138fc5bed9
mahesh-wohlig <mahesh@wohlig.com>
2015-04-22 11:24:59 +0530

    sum changes

--- bower_components/jquery/main4.js
+++ bower_components/jquery/main4.js
@@ -13,7 +13,11 @@
 	var bodyEl = document.body,
 		content = document.querySelector( '.content-wrap' ),
 		openbtn = document.getElementById( 'open-button' ),
+		openbtn2 = document.getElementById( 'open-button2' ),
+		openbtn3 = document.getElementById( 'open-button3' ),
 		closebtn = document.getElementById( 'close-button' ),
+		closebtn2 = document.getElementById( 'close-button2' ),
+		closebtn3 = document.getElementById( 'close-button3' ),
 		isOpen = false;
 
 	function init() {
@@ -22,8 +26,12 @@
 
 	function initEvents() {
 		openbtn.addEventListener( 'click', toggleMenu );
+		openbtn2.addEventListener( 'click', toggleMenu );
+		openbtn3.addEventListener( 'click', toggleMenu );
 		if( closebtn ) {
 			closebtn.addEventListener( 'click', toggleMenu );
+			closebtn2.addEventListener( 'click', toggleMenu );
+			closebtn3.addEventListener( 'click', toggleMenu );
 		}
 
 		// close the menu element if the target it´s not the menu element or one of its descendants..
